8bca9818fd9a2ce7557233e4ebc1a29c92b3e625
leejm1 <wnsaud5415@gamil.com>
2018-11-24 15:01:14 +0900

    local storage set

--- greetting.js
+++ greetting.js
@@ -5,6 +5,25 @@ const greeting = document.querySelector(".js-greeting")
 const USER_LOCAL_STORAGE = "currentUser";
 const SHOWING_CLASS_NAME = "showing";
 
+
+
+const askForName = () => {
+    form.classList.add(SHOWING_CLASS_NAME);
+    form.addEventListener("submit",handleSubmit);
+}
+
+let  handleSubmit = (event) => {
+    event.preventDefault();
+    const currentValue = form_input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+const saveName = (text) =>{
+    localStorage.setItem(USER_LOCAL_STORAGE,text);
+}
+
+
 const paintGreeting = text =>{
     form.classList.remove(SHOWING_CLASS_NAME);
     greeting.classList.add(SHOWING_CLASS_NAME);
@@ -15,7 +34,7 @@ const paintGreeting = text =>{
 const loadNm = () => {
     const currentUser = localStorage.getItem(USER_LOCAL_STORAGE);
     if(currentUser===null){
-    
+        askForName();
     } else {
         paintGreeting(currentUser);
     }
